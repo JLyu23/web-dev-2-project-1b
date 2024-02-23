397896ba81a331e4835ead81a4887f9361a30d48
Natalia N. <156234685+nat-my-nguyen@users.noreply.github.com>
2024-02-22 23:34:11 -0800

    Adjust Nav CSS  and JS script
    - tweaked the CSS for nav and footer
    - moved media queries to bottom of CSS
    - fixed JS due to buggy display of hamburger and menu

--- script/script.js
+++ script/script.js
@@ -1,11 +1,39 @@
 const menuButton = document.getElementById('menu-button');
 const menuNav = document.getElementById('menu-nav');
 
-menuButton.addEventListener('click', function () {
-    const openMenu = menuButton.getAttribute('aria-expanded');
-    
-    openMenu === 'false' ? 
-        (menuButton.setAttribute('aria-expanded', 'true'), menuNav.style.display = 'block')
-        : (menuButton.setAttribute('aria-expanded', 'false'), menuNav.style.display = 'none')
+function showNav() {
+    menuNav.style.display = 'block';
+};
 
+function hideNav() {
+    menuNav.style.display = 'none';
+};
+
+function hamburgerState(isOpen) {
+    menuButton.setAttribute('aria-expanded', isOpen.toString());
+};
+
+function toggleNav() {
+    const openMenu = menuButton.getAttribute('aria-expanded');
+
+    // openMenu === 'false' ? 
+    //     (menuButton.setAttribute('aria-expanded', 'true'), showNav())
+    //     : (menuButton.setAttribute('aria-expanded', 'false'), hideNav());
+
+    openMenu === 'false' ?
+        showNav()
+        : hideNav();
+}
+
+menuButton.addEventListener('click', function () {
+    toggleNav();
+    hamburgerState(menuNav.style.display === 'block');
+});
+
+window.addEventListener('resize', function() {
+    const windowWidth = window.innerWidth;
+
+    windowWidth > 768 ? 
+          (showNav(), hamburgerState(true)) 
+        : (hideNav(), hamburgerState(false));
 });
